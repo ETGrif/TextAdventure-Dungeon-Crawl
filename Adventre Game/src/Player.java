0ea8f8df145b92a9b0095f22f7fa4807295d86cc
ethan.griffin <ethan.griffin@MU-C1-09.mullen.pvt.k12.co.us>
2019-01-08 11:44:15 -0700

    player move function added, crashes when you return a string

--- Adventre Game/src/Player.java
+++ Adventre Game/src/Player.java
@@ -1,12 +1,56 @@
-
+import java.util.Scanner;
 public class Player
 	{
 
+		static Scanner userInput = new Scanner(System.in);
+		
 		private String name;
-		private int locX;
-		private int loxY;
+		static int playerX;
+		static int playerY;
 		private int hp;
 		
+		public static void move(){
+				//Find possible doors
+				String[] doors = {"North","West","East"};
+//				String[] doors = shortenedDoors(playerX, playerY);
+				System.out.println("Which direction do you move?");
+				for(int i = 0; i < doors.length; i++){
+					System.out.println((i+1)+": move "+doors[i]);
+				}
+				int response = askResponse(doors.length);
+				System.out.println("You chose to move "+doors[response-1]);
+				
+				
+		}
+		
+		public static String[] shortenedDoors(int x, int y){
+				String[] doors = Map.map[x][y].getDoors();
+				String[] shortenedDoors = new String[4-WorldBuilder.countClosedDoors()]; 
+				int index = 0;
+				for(String d : doors){
+					if(d != null){
+						shortenedDoors[index] = d;
+						index++;
+					}
+					
+				}
+		}
+		
+		public static int askResponse(int max){
+				int response;
+				try{
+					response = userInput.nextInt();
+					if(response > max){
+						System.out.println("Not a valid response, try again.");
+						response = askResponse(max);
+						
+					}
+				}catch(Exception e){
+					System.out.println("Not a valid response, try again.");
+					response = askResponse(max);
+				}
+				return response;
+		}
 		
 		
 		
